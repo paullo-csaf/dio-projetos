3db06e84b89ab9d10b2be1b584ef3bd649d0517c
biggie <paullo.p.c.f@hotmail.com>
2024-05-21 08:38:40 -0400

    repetição

---
+++ condicional/src/Repeticao.java
@@ -0,0 +1,15 @@
+public class Repeticao {
+    public static void main(String[] args) {
+        for(int i = 1; i < 10; i ++){
+            System.out.println("Contando carneirinhos " + i);
+        }
+    System.out.println("Joaozin dormiu... \n");
+
+
+
+        String champ [] = {"Pantheon", "Sett", "Yone", "Ornn"};
+        for(int x = 0; x < champ.length; x++){
+            System.out.println("O champ é:" + champ[x]);
+        }
+    }
+}
